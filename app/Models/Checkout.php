246e2eadb721ceecfb23da377d77ebe8669d5a0d
clavene <claudia.422022018@civitas.ukrida.ac.id>
2023-12-20 13:26:47 +0700

    Make n Migration: users, camps, camp_benefits, checkouts

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Checkout extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+    'user_id',
+    'camp_id',
+    // 'card_id',
+    // 'expired',
+    // 'cvc',
+    // 'is_paid',
+    // 'card_number',
+    ];
+
+//     public function setExpiredAttribute($value)
+// {
+//     $this->attributes['expired'] = date('Y-m-t', strtotime($value));
+// }
+
+public function camp(){
+    return $this->belongsTo(Camp::class, 'camp_id');
+}
+
+public function user(){
+    return $this->belongsTo(User::class, 'user_id');
+}
 }
